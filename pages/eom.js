c1601e885e9c6fec4f6e1fc86bdd1911080fdb12
cihat <cihat02020202@gmail.com>
2021-05-25 13:29:20 +0300

    initial commit

---
+++ pages/eom.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const eom = () => {
+  return (
+    <div className="page-container">
+      <div>
+        <h1>Employee Of The Month</h1>
+      </div>
+    </div>
+  )
+}
+
+export const getServerSideProps = async (pageContext) => {
+
+}
+
+export default eom
+
